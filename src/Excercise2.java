a04c2c7361f40488c4a9579ae967cb724236684f
s35023bebe <s35023@pjwstk.edu.pl>
2026-05-12 11:09:08 +0200

    Excercise 2

---
+++ src/Excercise2.java
@@ -0,0 +1,22 @@
+record ServiceOrder(String clientName, int hours, double hourRate) {}
+@FunctionalInterface
+interface PriceStrategy {
+    double calculate(ServiceOrder order);
+}
+class PriceCalculator {
+    public double calculate(ServiceOrder order, PriceStrategy strategy) {
+        return strategy.calculate(order);
+    }
+}
+public class Excercise2 {
+    public static void main(String[] args) {
+        ServiceOrder order = new ServiceOrder("Alpha Company", 10, 120.0);
+        PriceCalculator calculator = new PriceCalculator();
+        PriceStrategy standard = o -> o.hours() * o.hourRate();
+        PriceStrategy discount = o -> (o.hours() * o.hourRate()) * 0.90;
+        PriceStrategy weekend = o -> (o.hours() * o.hourRate()) * 1.25;
+        System.out.println("Standard: " + calculator.calculate(order, standard));
+        System.out.println("Discount: " + calculator.calculate(order, discount));
+        System.out.println("Weekend: " + calculator.calculate(order, weekend));
+    }
+}
